f4d897f11aba23909994129cb0d05d66b87d916a
richard-zysk <richard.andrew@zysk.tech>
2025-02-27 15:01:31 +0530

    fix: code refactor

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { PdfExtractionModule } from './pdf-extraction/pdf-extraction.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { EmailServiceModule } from './email-service/email-service.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports:[    MulterModule.register({
     dest: './uploads',
   }),
   PdfExtractionModule,
-  EmailServiceModule,],
+  EmailServiceModule,
+  AuthModule,],
   controllers: [],
   providers: [],
 })
