9e0a4779b84ac19ee6ffe8125ecd50e98c671ded
Srijita Das <srijita.das@zysk.tech>
2025-03-04 18:59:32 +0530

    Your commit message

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('ResuMate API')
+    .setDescription('The ResuMate API documentation')
     .setVersion('1.0')
-    .addTag('cats')
+    .addServer('http://localhost:3000', 'Local environment')
+    .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
-  await app.listen(8000);
+
+  await app.listen(3000);
+  
+  console.log(`
+API Postman collection: http://localhost:3000/api
+  `);
 }
 bootstrap();
